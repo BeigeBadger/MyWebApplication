8fc427a47a2622bbbb002a3adc9b75a24abf281c
Matt Stannett <matt.stannett@gmail.com>
2017-12-18 22:46:47 +1300

    Fix typo in controller name, spaces to tabs

--- MyWebApplication/App_Start/RouteConfig.cs
+++ MyWebApplication/App_Start/RouteConfig.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace MyWebApplication
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "MyHome", action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+			routes.MapRoute(
+				name: "Home",
+				url: "{controller}/{action}",
+				defaults: new { controller = "Home", action = "Index" }
+			);
+		}
+	}
 }
