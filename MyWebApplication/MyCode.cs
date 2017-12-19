8e605dd7453f6aeb90d2a4e2d14de52f02c3eae1
Matt Stannett <matt.stannett@gmail.com>
2017-12-20 07:10:30 +1300

    Add validation attributes and make setters public
    * The default modelbinder requires public setters to work.
    * Also added default constructor for use with the modelbinder.

--- MyWebApplication/MyCode.cs
+++ MyWebApplication/MyCode.cs
@@ -8,24 +8,33 @@ namespace MyWebApplication
 		/// <summary>
 		/// unique number which will be used to identify a gaming machine
 		/// </summary>
+		[Required]
+		[Range(0, Int64.MaxValue)]
 		[Display(Name = "Serial")]
-		public long SerialNumber { get; private set; }
+		public long SerialNumber { get; set; }
 
 		/// <summary>
 		/// range for this should be zero (default) to 1000 position
 		/// </summary>
+		[Required]
+		[Range(0, 1000)]
 		[Display(Name = "Position")]
-		public int MachinePosition { get; private set; } = 0;
+		public int MachinePosition { get; set; } = 0;
 
+		[Required]
 		[Display(Name = "Name")]
-		public string Name { get; private set; }
+		public string Name { get; set; }
 
 		[Display(Name = "Created at")]
 		[DisplayFormat(DataFormatString = "{0:O}")]         // ISO 8601
-		public DateTime CreatedAt { get; private set; }
+		public DateTime CreatedAt { get; set; }
 
 		[Display(Name = "Deleted")]
-		public bool IsDeleted { get; private set; }
+		public bool IsDeleted { get; set; }
+
+		public GamingMachine()
+		{
+		}
 
 		public GamingMachine(long serialNumber, int position, string name, DateTime createdAt, bool isDeleted)
 		{
