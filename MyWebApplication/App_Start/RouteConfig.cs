48b004b7ca2d5feb42996c6070ad678fbc4df3e6
Matt Stannett <matt.stannett@gmail.com>
2017-12-21 16:50:38 +1300

    Remove generic GamingMachines route as it will match automatically
    * Add specific routes for Edit and Delete actions so that the serialNumber will be passed through automatically

--- MyWebApplication/App_Start/RouteConfig.cs
+++ MyWebApplication/App_Start/RouteConfig.cs
@@ -10,15 +10,21 @@ namespace MyWebApplication
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapRoute(
-				name: "Home",
-				url: "{controller}/{action}",
-				defaults: new { controller = "Home", action = "Index" }
+				name: "Gaming Machines Edit",
+				url: "GamingMachines/{serialNumber}/Edit",
+				defaults: new { controller = "GamingMachines", action = "Edit" }
 			);
 
 			routes.MapRoute(
-				name: "Gaming Machines",
+				name: "Gaming Machines Delete",
+				url: "GamingMachines/{serialNumber}/Delete",
+				defaults: new { controller = "GamingMachines", action = "Delete" }
+			);
+
+			routes.MapRoute(
+				name: "Home",
 				url: "{controller}/{action}",
-				defaults: new { controller = "GamingMachines", action = "Index" }
+				defaults: new { controller = "Home", action = "Index" }
 			);
 
 			// TODO: Add more routes for CRUD operations
