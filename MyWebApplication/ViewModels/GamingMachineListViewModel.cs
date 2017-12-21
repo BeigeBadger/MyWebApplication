c26269d7d6e23834700e3b2bc6684337ab0b56b1
Matt Stannett <matt.stannett@gmail.com>
2017-12-21 21:39:21 +1300

    Remove dead code, update commentAdd ListVM

---
+++ MyWebApplication/ViewModels/GamingMachineListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyWebApplication.ViewModels
+{
+	public class GamingMachineListViewModel : GamingMachineBaseViewModel
+	{
+		[Required]
+		[Display(Name = "Created at")]
+		[DisplayFormat(DataFormatString = "{0:O}")]         // ISO 8601
+		public DateTime? CreatedAt { get; set; }
+
+		[Required]
+		[Display(Name = "Deleted")]
+		public bool? IsDeleted { get; set; }
+
+		public GamingMachineListViewModel(long serial, int position, string name, DateTime? createdAt, bool? isDeleted = false)
+			: base(serial, position, name)
+		{
+			CreatedAt = createdAt;
+			IsDeleted = isDeleted;
+		}
+	}
+}
